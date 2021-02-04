df8ca29ae2a93e1fde2202a869929c1c30634851
yaguma <yaguma34@gmail.com>
2021-02-04 22:01:34 +0900

    add logger

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,29 +9,35 @@ using samplemvc.Models;
 
 namespace samplemvc.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _logger = logger;
     }
+
+    public IActionResult Index()
+    {
+      _logger.LogTrace("Home Start.");
+      _logger.LogDebug("Home Start.");
+      _logger.LogInformation("Home Start.");
+      _logger.LogWarning("Home Start.");
+      _logger.LogError("Home Start.");
+      _logger.LogCritical("Home Start.");
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
--- Program.cs
+++ Program.cs
@@ -6,41 +6,43 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
 namespace samplemvc
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-                    .ConfigureLogging(logging => {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(LogLevel.Information);
-                    logging.AddAzureWebAppDiagnostics();
-                })
-                .ConfigureServices(serviceCollection =>
-                {
-                    serviceCollection
-                    .Configure<AzureFileLoggerOptions>(options =>
-                    {
-                        options.FileName = "azure-diagnostics-";
-                        options.FileSizeLimit = 50 * 1024;
-                        options.RetainedFileCountLimit = 5;
-                    })
-                    .Configure<AzureBlobLoggerOptions>(options =>
-                    {
-                        options.BlobName = "log.txt";
-                    });
-                })
-                .UseNLog();
+      CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+              webBuilder.UseStartup<Startup>();
+            })
+                .ConfigureLogging(logging =>
+                {
+                  logging.ClearProviders();
+                  logging.SetMinimumLevel(LogLevel.Information);
+                  logging.AddAzureWebAppDiagnostics();
+                })
+            // .ConfigureServices(serviceCollection =>
+            // {
+            //     serviceCollection
+            //     .Configure<AzureFileLoggerOptions>(options =>
+            //     {
+            //         options.FileName = "azure-diagnostics-";
+            //         options.FileSizeLimit = 50 * 1024;
+            //         options.RetainedFileCountLimit = 5;
+            //     })
+            //     .Configure<AzureBlobLoggerOptions>(options =>
+            //     {
+            //         options.BlobName = "log.txt";
+            //     });
+            // })
+            .UseNLog();
+  }
 }
